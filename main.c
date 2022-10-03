3cb4141833b728474b47296189a59c3c5de5c83d
champagnezup <113350157+champagnezup@users.noreply.github.com>
2022-10-03 12:27:20 +0200

    Add files via upload
    es1 tpsit con la freccia

--- main.c
+++ main.c
@@ -1,19 +1,10 @@
-//
-//  main.c
-//  es01TPSIT
-//  Creare un programma in linguaggio C che legga il file vgsales.csv e lo importi in un array di strutture.
-//  Ogni riga contiene i campi separati da virgole, per cui può essere comodo creare una funzione split
-//  che dalla riga letta ritorni la struttura valorizzata.
-//  Created by Alexandru Zup on 13/09/22.
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define DIM 1024
 
 typedef struct gioco{
-    
+
     int rank;
     char* name;
     char* platform;
@@ -30,10 +21,12 @@ typedef struct gioco{
 
 
 int main() {
-    
+
     gioco giochi[16600];
-    gioco *a = giochi;
-    
+    gioco *a;
+    a = giochi;
+    int k = 0;
+
     char riga[DIM];
     FILE*fp;
     fp = fopen("/Users/zupy/Documents/Scuola/TPSIT/file.csv","r");
@@ -44,36 +37,24 @@ int main() {
         printf("WARNING!: sono dentro");
         fgets(riga, DIM, fp);
         while(fgets(riga, DIM, fp) != EOF){
-            
-            (*a).rank = atoi(strtok(riga, ","));
-            (*a).name = strtok(NULL, ",");
-            (*a).platform = strtok(NULL, ",");
-            (*a).year = atoi(strtok(NULL, ","));
-            (*a).genre = strtok(NULL, ",");
-            (*a).publisher = strtok(NULL, ",");
-            (*a).NA_sales = atof(strtok(NULL, ","));
-            (*a).EU_sales = atof(strtok(NULL, ","));
-            (*a).JP_sales = atof(strtok(NULL, ","));
-            (*a).Other_sales = atof(strtok(NULL, ","));
-            (*a).Global_sales = atof(strtok(NULL, ","));
 
-            printf("\n %d, %s, %s, %d, %s, %s, %.3f, %.3f, %.3f, %.3f, %.3f", (*a).rank, (*a).name, (*a).platform, (*a).year, (*a).genre, (*a).publisher, (*a).NA_sales, (*a).EU_sales, (*a).JP_sales, (*a).Other_sales, (*a).Global_sales);
-            
-            a++;
+            (a +k)->rank = atoi(strtok(riga, ","));
+            (a +k)->name = strtok(NULL, ",");
+            (a +k)->platform = strtok(NULL, ",");
+            (a +k)->year = atoi(strtok(NULL, ","));
+            (a +k)->genre = strtok(NULL, ",");
+            (a +k)->publisher = strtok(NULL, ",");
+            (a +k)->NA_sales = atof(strtok(NULL, ","));
+            (a +k)->EU_sales = atof(strtok(NULL, ","));
+            (a +k)->JP_sales = atof(strtok(NULL, ","));
+            (a +k)->Other_sales = atof(strtok(NULL, ","));
+            (a +k)->Global_sales = atof(strtok(NULL, ","));
+
+            printf("\n %d, %s, %s, %d, %s, %s, %.3f, %.3f, %.3f, %.3f, %.3f", (a +k)->rank, (a +k)->name, (a +k)->platform, (a +k)->year, (a +k)->genre, (a +k)->publisher, (a +k)->NA_sales, (a +k)->EU_sales, (a +k)->JP_sales, (a +k)->Other_sales, (a +k)->Global_sales);
+
+            k++;
         }
         fclose(fp);
     }
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
